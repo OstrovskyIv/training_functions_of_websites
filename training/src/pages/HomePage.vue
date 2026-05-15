3fb663b896b1a34e02026ae4498730f9d293dfc6
OstrovskyIv <ostrovskyiml@gmail.com>
2026-05-15 10:44:40 +0300

    Убрал выделение со страницы

--- training/src/pages/HomePage.vue
+++ training/src/pages/HomePage.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="h-screen flex flex-col items-center justify-center px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-16">
-    <h1 class="select-none text-center font-angellin sm:text-[1.25rem] md:text-[1.5rem] lg:text-[1.75rem] xl:text-[2rem] 2xl:text-[2.5rem]">
+  <div class="select-none h-screen flex flex-col items-center justify-center px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-16">
+    <h1 class="text-center font-angellin sm:text-[1.25rem] md:text-[1.5rem] lg:text-[1.75rem] xl:text-[2rem] 2xl:text-[2.5rem]">
       Workout notebook
     </h1>
     <button class="">
