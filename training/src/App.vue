3cce9698520f73326668cebcea3fea808ca7d831
OstrovskyIv <ostrovskyiml@gmail.com>
2026-05-15 10:43:25 +0300

    Добавил шрифт и фон для главной страницы

--- training/src/App.vue
+++ training/src/App.vue
@@ -1,3 +1,11 @@
+<script setup>
+import backgroundImage from '@/shared/images/background.webp'
+</script>
+
+
 <template>
-      <router-view />
+  <div class="min-h-screen bg-cover bg-center bg-fixed"
+       :style="{ backgroundImage: `url(${backgroundImage})` }">
+    <router-view />
+  </div>
 </template>
