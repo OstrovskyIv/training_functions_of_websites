20ca393df019ba034d9fadd021356b1d20e2ea12
OstrovskyIv <ostrovskyiml@gmail.com>
2026-05-26 22:10:03 +0300

    добавил вторую кнопку

--- training/src/pages/HomePage.vue
+++ training/src/pages/HomePage.vue
@@ -38,6 +38,32 @@
       </button>
     </div>
 
+    <div class="flex flex-col gap-2 sm:gap-3 md:gap-4 lg:gap-5 xl:gap-6 2xl:gap-8">
+      <button class="
+      px-4 py-2 text-sm shadow-[0_4px_0_0_rgb(160_98_0)]
+      sm:px-5 sm:py-2.5 sm:text-base sm:shadow-[0_5px_0_0_rgb(160_98_0)]
+      md:px-6 md:py-3 md:text-lg md:shadow-[0_6px_0_0_rgb(160_98_0)]
+      lg:px-7 lg:py-3.5 lg:text-xl lg:shadow-[0_7px_0_0_rgb(160_98_0)]
+      xl:px-8 xl:py-4 xl:text-2xl xl:shadow-[0_8px_0_0_rgb(160_98_0)]
+      2xl:px-10 2xl:py-5 2xl:text-3xl 2xl:shadow-[0_10px_0_0_rgb(160_98_0)]
+      tracking-wider font-bold font-angellin
+      bg-gradient-to-b from-yellow-400 to-yellow-600
+      rounded-lg
+      hover:from-yellow-500 hover:to-yellow-700
+      hover:shadow-[0_2px_0_0_rgb(160_98_0)]
+      hover:translate-y-1
+      active:scale-95
+      transition-all duration-150
+      cursor-pointer
+      w-auto
+    ">
+        Buttons
+      </button>
+
+      <button class="">
+        Logics
+      </button>
+    </div>
   </div>
 </template>
 
