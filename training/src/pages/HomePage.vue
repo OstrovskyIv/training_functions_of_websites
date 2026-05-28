d538200713ae7730109c8388dbeeac961e20be74
OstrovskyIv <ostrovskyiml@gmail.com>
2026-05-28 22:10:06 +0300

    обноаил цвет второй кнопки

--- training/src/pages/HomePage.vue
+++ training/src/pages/HomePage.vue
@@ -34,19 +34,19 @@
       </button>
 
       <button class="
-      px-4 py-2 text-sm shadow-[0_4px_0_0_rgb(140_70_0)]
-      sm:px-5 sm:py-2.5 sm:text-base sm:shadow-[0_5px_0_0_rgb(140_70_0)]
-      md:px-6 md:py-3 md:text-lg md:shadow-[0_6px_0_0_rgb(140_70_0)]
-      lg:px-7 lg:py-3.5 lg:text-xl lg:shadow-[0_7px_0_0_rgb(140_70_0)]
-      xl:px-8 xl:py-4 xl:text-2xl xl:shadow-[0_8px_0_0_rgb(140_70_0)]
-      2xl:px-10 2xl:py-5 2xl:text-3xl 2xl:shadow-[0_10px_0_0_rgb(140_70_0)]
+      px-4 py-2 text-sm shadow-[0_4px_0_0_rgb(160_98_0)]
+      sm:px-5 sm:py-2.5 sm:text-base sm:shadow-[0_5px_0_0_rgb(160_98_0)]
+      md:px-6 md:py-3 md:text-lg md:shadow-[0_6px_0_0_rgb(160_98_0)]
+      lg:px-7 lg:py-3.5 lg:text-xl lg:shadow-[0_7px_0_0_rgb(160_98_0)]
+      xl:px-8 xl:py-4 xl:text-2xl xl:shadow-[0_8px_0_0_rgb(160_98_0)]
+      2xl:px-10 2xl:py-5 2xl:text-3xl 2xl:shadow-[0_10px_0_0_rgb(160_98_0)]
       tracking-wider font-bold font-angellin
-      bg-gradient-to-b from-orange-500 to-orange-700
+      bg-gradient-to-b from-amber-500 to-amber-700
       rounded-lg
       cursor-pointer
       w-auto
-      hover:from-orange-600 hover:to-orange-800
-      hover:shadow-[0_2px_0_0_rgb(140_70_0)]
+      hover:from-amber-600 hover:to-amber-800
+      hover:shadow-[0_2px_0_0_rgb(160_98_0)]
       transition-all duration-150
       hover:translate-y-1
       active:scale-95
