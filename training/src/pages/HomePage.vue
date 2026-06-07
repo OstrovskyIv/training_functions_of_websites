299234ea541e00b4f3331d31dad5cc788b972be0
OstrovskyIv <ostrovskyiml@gmail.com>
2026-06-07 22:36:37 +0300

    Изменил цвет кнопки "Logics"

--- training/src/pages/HomePage.vue
+++ training/src/pages/HomePage.vue
@@ -35,15 +35,16 @@
       </button>
 
       <button class="
-      px-4 py-2 text-sm shadow-[0_4px_0_0_rgb(160_98_0)]
-      sm:px-5 sm:py-2.5 sm:text-base sm:shadow-[0_5px_0_0_rgb(160_98_0)]
-      md:px-6 md:py-3 md:text-lg md:shadow-[0_6px_0_0_rgb(160_98_0)]
-      lg:px-7 lg:py-3.5 lg:text-xl lg:shadow-[0_7px_0_0_rgb(160_98_0)]
-      xl:px-8 xl:py-4 xl:text-2xl xl:shadow-[0_8px_0_0_rgb(160_98_0)]
-      2xl:px-10 2xl:py-5 2xl:text-3xl 2xl:shadow-[0_10px_0_0_rgb(160_98_0)]
+      px-4 py-2 text-sm shadow-[0_4px_0_0_rgb(77_124_15)]
+      sm:px-5 sm:py-2.5 sm:text-base sm:shadow-[0_5px_0_0_rgb(77_124_15)]
+      md:px-6 md:py-3 md:text-lg md:shadow-[0_6px_0_0_rgb(77_124_15)]
+      lg:px-7 lg:py-3.5 lg:text-xl lg:shadow-[0_7px_0_0_rgb(77_124_15)]
+      xl:px-8 xl:py-4 xl:text-2xl xl:shadow-[0_8px_0_0_rgb(77_124_15)]
+      2xl:px-10 2xl:py-5 2xl:text-3xl 2xl:shadow-[0_10px_0_0_rgb(77_124_15)]
       tracking-wider font-bold font-angellin
-      bg-gradient-to-b from-yellow-400 to-yellow-600
+      bg-gradient-to-b from-lime-400 to-lime-600
       rounded-lg
+      text-white
 ">
         Logics
       </button>
