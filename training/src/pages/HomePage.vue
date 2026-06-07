0b2929e81abbc20d97e851979fb624e6bcd435bf
OstrovskyIv <ostrovskyiml@gmail.com>
2026-06-07 22:26:21 +0300

    Обновил базу кнопки "Logics"

--- training/src/pages/HomePage.vue
+++ training/src/pages/HomePage.vue
@@ -34,7 +34,17 @@
         Buttons
       </button>
 
-      <button class="">
+      <button class="
+      px-4 py-2 text-sm shadow-[0_4px_0_0_rgb(160_98_0)]
+      sm:px-5 sm:py-2.5 sm:text-base sm:shadow-[0_5px_0_0_rgb(160_98_0)]
+      md:px-6 md:py-3 md:text-lg md:shadow-[0_6px_0_0_rgb(160_98_0)]
+      lg:px-7 lg:py-3.5 lg:text-xl lg:shadow-[0_7px_0_0_rgb(160_98_0)]
+      xl:px-8 xl:py-4 xl:text-2xl xl:shadow-[0_8px_0_0_rgb(160_98_0)]
+      2xl:px-10 2xl:py-5 2xl:text-3xl 2xl:shadow-[0_10px_0_0_rgb(160_98_0)]
+      tracking-wider font-bold font-angellin
+      bg-gradient-to-b from-yellow-400 to-yellow-600
+      rounded-lg
+">
         Logics
       </button>
 
