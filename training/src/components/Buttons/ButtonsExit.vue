3982a9cca7149bea835c5f880654c7e4e67293d9
OstrovskyIv <ostrovskyiml@gmail.com>
2026-06-08 00:04:17 +0300

    Добавил новую кнопку выхода (реализовал базу)

---
+++ training/src/components/Buttons/ButtonsExit.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <button class="
+      px-4 py-2 text-sm shadow-[0_4px_0_0_rgb(220_38_38)]
+      sm:px-5 sm:py-2.5 sm:text-base
+      md:px-6 md:py-3 md:text-lg
+      lg:px-7 lg:py-3.5 lg:text-xl
+      xl:px-8 xl:py-4 xl:text-2xl
+      tracking-wider font-bold font-angellin
+      bg-gradient-to-b from-red-400 to-red-600
+      rounded-lg
+      text-white
+      cursor-pointer
+    ">
+    Exit
+  </button>
+</template>
